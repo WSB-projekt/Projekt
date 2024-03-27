22b6db6f47dd46bdac540763a7c1abb6da8cf044
klaudiastarczew <kstarczew@gmail.com>
2024-03-27 22:37:20 +0100

    connect backend with frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from 'react';
+
 export default function App() {
+	const [message, setMessage] = useState('');
+
+	useEffect(() => {
+		async function getTasks() {
+			const res = await fetch('/api/tasklistify');
+			const tasks = await res.json();
+
+			setMessage(tasks.mssg);
+		}
+		getTasks();
+	}, []);
+
 	return (
-		<main className="container">
+		<main className='container'>
 			<h1>Hello world</h1>
+			{message && <p>{message}</p>}
 		</main>
 	);
 }
-
--- server/routes.js
+++ server/routes.js
@@ -3,23 +3,23 @@ const express = require('express');
 const router = express.Router();
 
 // GET /tasklistify
-router.get('/tasklistify', async (req, res) => {
-	res.status(200).json({ msg: 'GET REQUEST TO /api/tasklistify' });
+router.get('/tasklistify', (req, res) => {
+	res.status(200).json({ mssg: 'GET REQUEST TO /api/tasklistify' });
 });
 
 // POST /tasklistify
-router.get('/tasklistify', async (req, res) => {
-	res.status(201).json({ msg: 'POST REQUEST TO /api/tasklistify' });
+router.post('/tasklistify', (req, res) => {
+	res.status(201).json({ mssg: 'POST REQUEST TO /api/tasklistify' });
 });
 
 // DELETE /tasklistify/:id
-router.get('/tasklistify', async (req, res) => {
-	res.status(200).json({ msg: 'DELETE REQUEST TO /api/tasklistify/:id' });
+router.delete('/tasklistify/:id', (req, res) => {
+	res.status(200).json({ mssg: `DELETE REQUEST TO /api/tasklistify/:id` });
 });
 
 // PUT /tasklistify/:id
-router.get('/tasklistify', async (req, res) => {
-	res.status(200).json({ msg: 'PUT REQUEST TO /api/tasklistify/:id' });
+router.put('/tasklistify/:id', (req, res) => {
+	res.status(200).json({ mssg: `PUT REQUEST TO /api/tasklistify/:id` });
 });
 
 module.exports = router;
