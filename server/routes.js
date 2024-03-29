50849fd997cbce5d4a0dcae8d580704b80a05eb6
zajac-paulinaa <bye.zajac@gmail.com>
2024-03-29 22:37:21 +0100

    creating mongodb queries

--- server/routes.js
+++ server/routes.js
@@ -1,25 +1,51 @@
 const express = require('express');
-
 const router = express.Router();
+const { getConnectedClient } = require("./database");
+const { ObjectId } = require("mongodb");
+
+const getCollection = () => {
+    const client = getConnectedClient();
+    const collection = client.db("tasklistifydb").collection("tasklistify");
+    return collection;
+}
 
 // GET /tasklistify
-router.get('/tasklistify', (req, res) => {
-	res.status(200).json({ mssg: 'GET REQUEST TO /api/tasklistify' });
+router.get('/tasklistify', async (req, res) => {
+	const collection = getCollection();
+	const tasklistify = await collection.find({}).toArray();
+
+	res.status(200).json(tasklistify);
 });
 
 // POST /tasklistify
-router.post('/tasklistify', (req, res) => {
-	res.status(201).json({ mssg: 'POST REQUEST TO /api/tasklistify' });
+router.post('/tasklistify', (async (req, res) => {
+    const collection = getCollection();
+    const { tasklistify } = req.body;
+
+	const NewTasklitify = await collection.insertOne({tasklistify, satus: false});
+
+	res.status(201).json({ tasklistify, status: false, _id: NewTasklitify.insertedId });
 });
 
 // DELETE /tasklistify/:id
-router.delete('/tasklistify/:id', (req, res) => {
-	res.status(200).json({ mssg: `DELETE REQUEST TO /api/tasklistify/:id` });
+router.delete('/tasklistify/:id', async (req, res) => {
+    const collection = getCollection();
+    const _id = new ObjectId(req.params.id);
+
+	const deletedTasklisify = await collection.deleteOne({ _id });
+
+	res.status(200).json(deletedTasklisify);
 });
 
 // PUT /tasklistify/:id
-router.put('/tasklistify/:id', (req, res) => {
-	res.status(200).json({ mssg: `PUT REQUEST TO /api/tasklistify/:id` });
+router.put('/tasklistify/:id', async (req, res) => {
+	const collection = getCollection();
+    const _id = new ObjectId(req.params.id);
+	const { status } = req.body;
+
+	const updatedTasklistify = await collection.updateOne({ _id }, { $set: { status: !status}})
+
+	res.status(200).json(updatedTasklistify);
 });
 
 module.exports = router;
